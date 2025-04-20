e5810a472b5d5d1d438df53e04d3656ed81fb67b
Pavel <pasha0801@gmail.com>
2025-04-20 17:45:08 +0300

    PMS-6: refactor view features

---
+++ frontend/src/view/features/Boards/components/BoardsCard.tsx
@@ -0,0 +1,70 @@
+import { Board } from '#shared/types';
+import { Typography, Card, CardContent, Box } from '@mui/material';
+
+type CardElementProps = {
+  board: Board;
+  onClick: () => void;
+};
+
+export const BoardsCard = ({ board, onClick }: CardElementProps) => {
+  return (
+    <Card
+      variant="elevation"
+      sx={{
+        transition: 'all 0.1s ease',
+        '&:hover': {
+          transform: 'scale(1.0007)',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+          zIndex: 1,
+        },
+      }}
+    >
+      <CardContent
+        onClick={onClick}
+        sx={{
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: 2,
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <Typography
+            onClick={onClick}
+            variant="body1"
+            sx={{
+              cursor: 'pointer',
+              textDecoration: 'none',
+              fontWeight: 'bold',
+            }}
+          >
+            {board.name}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              textDecoration: 'none',
+              color: 'text.secondary',
+            }}
+          >
+            {board.description}
+          </Typography>
+        </Box>
+
+        <Typography
+          onClick={onClick}
+          variant="body1"
+          sx={{
+            cursor: 'pointer',
+            textDecoration: 'none',
+            color: 'primary.main',
+            '&:hover': { color: 'primary.light' },
+          }}
+        >
+          Перейти к доске
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
